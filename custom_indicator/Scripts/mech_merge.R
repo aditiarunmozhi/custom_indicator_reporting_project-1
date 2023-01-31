bfc3552b81845f2842fe361334442343eee93148
aditiarunmozhi <aditiarunmozhi@gmail.com>
2023-01-31 14:38:19 -0500

    data validation tables

--- custom_indicator/Scripts/mech_merge.R
+++ custom_indicator/Scripts/mech_merge.R
@@ -1,2 +1,10 @@
 #merge with mech ref file
-mech_merge <- left_join(kp_disaggs_counts_clean, mech_id_ref_table_fy23, by = c("Country" ="country"))
+mech_complete_clean_data <- left_join(complete_clean_data, mech_id_ref_table_fy23, by = c("Country" = "country")) %>%
+  relocate(mech_code:mech_name, .after = "reportingperiod")
+
+#data validation with infolink
+country_kp <- mech_complete_clean_data %>% filter(population %in% keypop & indicator %in% c("TX_NEW_VERIFY","TX_RTT_VERIFY","PrEP_OFFER","PrEP_NEW_VERIFY","PrEP_CT_VERIFY")) %>% 
+  group_by(reportingperiod, Country, indicator) %>% summarize(value = sum(value))
+
+country_age_sex <- mech_complete_clean_data %>% filter(Country == "Burkina Faso" & indicator == "TX_NEW_VERIFY") %>% 
+  group_by(Country) %>% summarize(value = sum(value))
