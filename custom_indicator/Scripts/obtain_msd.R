0fe81d28454e6f31ca4c31a94501faa555b0cbe0
Bourke Betz <64644141+BBetz907@users.noreply.github.com>
2023-02-02 07:59:49 -0800

    update to pull OU-level data since geographic information not necessary here

--- custom_indicator/Scripts/obtain_msd.R
+++ custom_indicator/Scripts/obtain_msd.R
@@ -1,6 +1,4 @@
-update.packages("gagglr")
 
-# set_pano("aarunmozhi@usaid.gov")
 load_secrets()
 
 # set_paths(folderpath_msd = "Data",
@@ -35,11 +33,13 @@ dest_path <- paste0(si_path(),"/Temp/")
 
 
 # pull latest psnuXim MSD ---------------------------------------------------------
-url_psnu_im <- mer_items %>%
+url_ou_im <- mer_items %>%
   filter(type == "file zip_file",
-         str_detect(item, ".*_PSNU_IM_FY2.*.zip$")) %>%
+         str_detect(item, ".*_OU_IM_FY2.*.zip$")) %>%
   pull(path) %>%
   first() 
 
+
+
 # quick fix to filepaths --------------------------------------------------
-grabr::pano_download(item_url = url_psnu_im, session = sess)
+grabr::pano_download(item_url = url_ou_im, session = sess)
