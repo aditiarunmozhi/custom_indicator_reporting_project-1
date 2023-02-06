c4ad49f3a7cca1a26f14a1e349768a0c62a6ffb4
aditiarunmozhi <aditiarunmozhi@gmail.com>
2023-02-06 18:56:55 -0500

    data checking update

--- custom_indicator/Scripts/mech_merge.R
+++ custom_indicator/Scripts/mech_merge.R
@@ -4,10 +4,15 @@ mech_complete_clean_data <- left_join(complete_clean_data, mech_id_ref_table_fy2
 
 #data validation with infolink
 #kp data
-country_kp <- mech_complete_clean_data %>% filter(population %in% keypop & indicator %in% c("TX_NEW_VERIFY","TX_RTT_VERIFY","PrEP_OFFER","PrEP_NEW_VERIFY","PrEP_CT_VERIFY")) %>% 
-  dplyr::group_by(reportingperiod, country, indicator) %>% dplyr::summarize(value = sum(value))
+country_kp_count <- mech_complete_clean_data %>% filter(population %in% keypop & indicator %in% c("TX_NEW_VERIFY","TX_RTT_VERIFY","PrEP_OFFER","PrEP_NEW_VERIFY","PrEP_CT_VERIFY")) %>% 
+  dplyr::group_by(reportingperiod, country, indicator, ) %>% dplyr::summarize(value = sum(value))
+
+country_kp_snapshot <- mech_complete_clean_data %>% filter(population== "MSM" & indicator %in% c("TX_CURR_VERIFY", "TX_PVLS_VERIFY", "TX_PVLS_ELIGIBLE")) %>% 
+  dplyr::group_by(reportingperiod, country, indicator, population) %>% dplyr::summarize(value = sum(value))
 
 #non-kp data
+country_age_sex_count <- mech_complete_clean_data %>% filter(!(population %in% keypop) & indicator == "TX_NEW_VERIFY") %>% 
+  dplyr::group_by(reportingperiod, country, indicator) %>% dplyr::summarize(value = sum(value))
 
-country_age_sex <- mech_complete_clean_data %>% filter(!(population %in% keypop) & indicator == "TX_NEW_VERIFY") %>% 
-  group_by(reportingperiod, country, indicator) %>% summarize(value = sum(value))
+country_age_sex_snapshot <- mech_complete_clean_data %>% filter(!(population %in% keypop) & indicator == "TX_CURR_VERIFY") %>% 
+  dplyr::group_by(reportingperiod, country, indicator) %>% dplyr::summarize(value = sum(value))
