5ad09682cb7b7e7d2ff49a03600b6a29a3b4e463
Bourke Betz <64644141+BBetz907@users.noreply.github.com>
2023-02-09 08:06:02 -0800

    CAR coding

---
+++ custom_indicator/Scripts/orgunits_matched_KGZ.R
@@ -0,0 +1,79 @@
+
+# obtain metadata from infolink -------------------------------------------
+
+
+#ideally we'd run this after mech ref table merge and before data checking. First the 
+#issue with the data source must be solved for mech_ref...R
+# kgz_info <- complete_clean_data %>% filter(country=="Nepal") %>% 
+#   select(snu_1:snu_3_id, value) %>% group_by(across(c(-value))) %>% summarise(value = sum(value), .groups = "drop") %>% 
+#   clean_names() %>% 
+#   glimpse()
+
+# user purr to create DF for each country, named after each count --------
+kgz_info <- complete_clean_data %>% filter(country=="Kyrgyzstan") %>%
+  group_by(across(-c(value))) %>% summarise(value = sum(value), .groups = "drop") %>% 
+  mutate(snu_4 = str_to_title(snu_4),
+         snu_3 = str_to_title(snu_3),
+         snu_2 = str_to_title(snu_2),
+         snu_4  = str_replace(snu_4, "  ", " "),
+         snu_3  = str_replace(snu_3, "  ", " "),
+         snu_2  = str_replace(snu_2, "  ", " ")) %>%
+  clean_names() %>% 
+  print()
+
+table(kgz_info$snu_3) #snu3 level should match to level 7 in datim
+
+
+# get orgunit levels to match and join ------------------------------------
+kgz7op <- kgz_7_8 %>% filter(orgunit_level  == "7") %>% select(orgunit_level:orgunit_name) 
+kgz7uid <- c(kgz7op$orgunit_uid)
+
+kgz8op <- kgz_7_8 %>% filter(orgunit_level  == "8") %>% select(orgunit_level:orgunit_name)
+kgz8uid <- c(kgz8op$orgunit_uid)
+
+
+################################################################################
+
+
+
+# for most level 2 that match_level 7, use snu_3 -----------------------
+kgz7<- kgz_info %>% filter(snu_2_id %in% kgz7uid, snu_2!="") %>% select(-snu_1_id, -snu_3_id) %>% rename(orgunit = snu_2, orgunituid = snu_2_id) 
+scales::percent(nrow(kgz7)/nrow(kgz_info))
+
+# for level 2 that doesn't match level 7, match by snu_3 id --------
+kgz7m1 <- kgz_info %>% filter(!snu_2_id %in% kgz7uid, snu_2 != "") %>% rename(orgunit_name = snu_3)
+scales::percent(nrow(kgz7m1)/nrow(kgz_info))
+nrow(kgz7m1)
+
+#identify and resolve any failed matches
+kgz71 <- kgz7m1 %>%
+  anti_join(kgz7op) %>% print()
+#resolve discrepancies
+
+
+#now match on level 2 names
+kgz7m <- kgz7m1 %>% inner_join(kgz7op) %>% # or inner if there are non-matches 
+  select(-snu_1_id, -snu_3_id) %>%
+  rename(orgunituid = orgunit_uid, orgunit = orgunit_name) %>%
+  print() #check if the tibble nrow matches the previous count. if it exceeds there is some double matching
+
+
+#check for 1:many matches
+kgz7m %>% select(value, indicator, age, sex, otherdisaggregate, numdenom, population, orgunit) %>% group_by_all() %>% 
+  filter(n()>1)
+
+
+#check for unmatched
+kgz7m %>% filter(is.na(orgunituid)) 
+
+
+
+
+
+kgz <- bind_rows(kgz7, kgz7m) %>% select(-contains("snu"), -orgunit_level) %>% 
+  print() 
+#check to see if number of rows matches source
+nrow(kgz) - nrow(kgz_info)
+
+
+#later bind country dfs together
---
+++ custom_indicator/Scripts/orgunits_matched_TJK.R
@@ -0,0 +1,77 @@
+
+# obtain metadata from infolink -------------------------------------------
+
+
+#ideally we'd run this after mech ref table merge and before data checking. First the 
+#issue with the data source must be solved for mech_ref...R
+# tjk_info <- complete_clean_data %>% filter(country=="Nepal") %>% 
+#   select(snu_1:snu_3_id, value) %>% group_by(across(c(-value))) %>% summarise(value = sum(value), .groups = "drop") %>% 
+#   clean_names() %>% 
+#   glimpse()
+
+# user purr to create DF for each country, named after each count --------
+tjk_info <- complete_clean_data %>% filter(country=="Tajikistan") %>%
+  mutate(snu_4 = str_to_title(snu_4),
+         snu_3 = str_to_title(snu_3),
+         snu_2 = str_to_title(snu_2),
+         snu_4  = str_replace(snu_4, "  ", " "),
+         snu_3  = str_replace(snu_3, "  ", " "),
+         snu_2  = str_replace(snu_2, "  ", " ")) %>%
+  clean_names() %>% print()
+
+table(tjk_info$snu_3) #snu3 level should match to level 7 in datim
+
+
+# get orgunit levels to match and join ------------------------------------
+tjk7op <- tjk_6_7 %>% filter(orgunit_level  == "7") %>% select(orgunit_level:orgunit_name) 
+tjk7uid <- c(tjk7op$orgunit_uid)
+
+tjk6op <- tjk_6_7 %>% filter(orgunit_level  == "6") %>% select(orgunit_level:orgunit_name)
+tjk6uid <- c(tjk6op$orgunit_uid)
+
+
+################################################################################
+
+
+
+# for most level 3 that match_level 7, use snu_3_id -----------------------
+tjk7<- tjk_info %>% filter(snu_3_id %in% tjk7uid, snu_3!="") %>% select(-snu_1_id:-snu_2_id) %>% rename(orgunit = snu_3, orgunituid = snu_3_id) 
+scales::percent(nrow(tjk7)/nrow(tjk_info))
+
+# for level 3 that doesn't match level 7, match by snu_3 id --------
+tjk7m1 <- tjk_info %>% filter(!snu_3_id %in% tjk7uid, snu_3 != "") %>% rename(orgunit_name = snu_3)
+scales::percent(nrow(tjk7m1)/nrow(tjk_info))
+nrow(tjk7m1)
+
+#identify and resolve any failed matches
+tjk61 <- tjk7m1 %>%
+  anti_join(tjk7op) %>% print()
+#resolve discrepancies
+
+
+#now match
+tjk7m <- tjk7m1 %>% inner_join(tjk7op) %>% # or inner if there are non-matches 
+  select(-snu_1_id:-snu_3_id) %>%
+  rename(orgunituid = orgunit_uid, orgunit = orgunit_name) %>%
+  print() #check if the tibble nrow matches the previous count. if it exceeds there is some double matching
+
+
+#check for 1:many matches
+tjk7m %>% select(value, indicator, age, sex, otherdisaggregate, numdenom, population, orgunit) %>% group_by_all() %>% 
+  filter(n()>1)
+
+
+#check for unmatched
+tjk7m %>% filter(is.na(orgunituid)) 
+
+
+
+
+
+tjk <- bind_rows(tjk7, tjk7m) %>% select(-contains("snu"), -orgunit_level) %>% 
+  print() 
+#check to see if number of rows matches source
+nrow(tjk) - nrow(tjk_info)
+
+
+#later bind country dfs together
