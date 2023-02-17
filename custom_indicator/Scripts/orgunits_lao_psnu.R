0e2a2fe6a2f624d2a20140249b8391ec3e36faa5
aditiarunmozhi <aditiarunmozhi@gmail.com>
2023-02-17 18:52:44 -0500

    laos orgunit table update

---
+++ custom_indicator/Scripts/orgunits_lao_psnu.R
@@ -0,0 +1,19 @@
+
+#transform and create table
+lao_orgs_clean <- lao_orgs %>% select(-c(regionorcountry_code, regionorcountry_name, moh_id, orgunit_code))
+
+level_5 <- lao_orgs_clean %>% filter(orgunit_level == 5) %>% 
+  rename(orgunit_4 = orgunit_parent, orgunit_4_uid = orgunit_parent_uid, orgunit_5_uid = orgunit_uid, orgunit_5 = orgunit_name) %>%
+  select(-orgunit_level)
+level_6 <- lao_orgs_clean %>% filter(orgunit_level == 6) %>%
+  rename(orgunit_5 = orgunit_parent, orgunit_5_uid = orgunit_parent_uid, orgunit_6_uid = orgunit_uid, orgunit_6 = orgunit_name) %>%
+  select(-orgunit_level)
+level_7 <- lao_orgs_clean %>% filter(orgunit_level == 7) %>%
+  rename(orgunit_6 = orgunit_parent, orgunit_6_uid = orgunit_parent_uid, orgunit_7_uid = orgunit_uid, orgunit_7 = orgunit_name) %>%
+  select(-orgunit_level)
+
+lao_orgunit_table <- full_join(level_5, level_6, by = join_by(orgunit_5_uid, orgunit_5), multiple = "all") %>%
+  full_join(level_7, by = join_by(orgunit_6_uid, orgunit_6), multiple = "all")
+
+#merge with data
+
---
+++ custom_indicator/Scripts/orgunits_lao_read_dfs.R
@@ -0,0 +1,3 @@
+lao <- read.csv("Data/laos_custom_data.csv")
+lao_orgs <- read.csv("Data/laos_orgunits.csv")
+orgunit_levels <- read.csv("Data/orgunit_levels_by_ou.csv")
