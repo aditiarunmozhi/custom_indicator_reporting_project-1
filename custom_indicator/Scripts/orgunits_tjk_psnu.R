8795cee82eaf65e4ce431bea06ad87821770a4ba
aditiarunmozhi <aditiarunmozhi@gmail.com>
2023-02-21 13:13:20 -0500

    update code for lao and tjk

--- custom_indicator/Scripts/orgunits_tjk_psnu.R
+++ custom_indicator/Scripts/orgunits_tjk_psnu.R
@@ -1,18 +1,18 @@
 #transform and create table
 tjk_orgs_clean <- tjk_orgs %>% select(-c(regionorcountry_code, regionorcountry_name, moh_id, orgunit_code))
 
-level_5 <- tjk_orgs_clean %>% filter(orgunit_level == 5) %>% 
+tjk_level_5 <- tjk_orgs_clean %>% filter(orgunit_level == 5) %>% 
   rename(orgunit_4 = orgunit_parent, orgunit_4_uid = orgunit_parent_uid, orgunit_5_uid = orgunit_uid, orgunit_5 = orgunit_name) %>%
   select(-orgunit_level)
-level_6 <- tjk_orgs_clean %>% filter(orgunit_level == 6) %>%
+tjk_level_6 <- tjk_orgs_clean %>% filter(orgunit_level == 6) %>%
   rename(orgunit_5 = orgunit_parent, orgunit_5_uid = orgunit_parent_uid, orgunit_6_uid = orgunit_uid, orgunit_6 = orgunit_name) %>%
   select(-orgunit_level)
-level_7 <- tjk_orgs_clean %>% filter(orgunit_level == 7) %>%
+tjk_level_7 <- tjk_orgs_clean %>% filter(orgunit_level == 7) %>%
   rename(orgunit_6 = orgunit_parent, orgunit_6_uid = orgunit_parent_uid, orgunit_7_uid = orgunit_uid, orgunit_7 = orgunit_name) %>%
   select(-orgunit_level)
 
-tjk_orgunit_table <- full_join(level_5, level_6, by = join_by(orgunit_5_uid, orgunit_5), multiple = "all") %>%
-  full_join(level_7, by = join_by(orgunit_6_uid, orgunit_6), multiple = "all") %>% select(sort(colnames(.))) %>%
+tjk_orgunit_table <- full_join(tjk_level_5, tjk_level_6, by = join_by(orgunit_5_uid, orgunit_5), multiple = "all") %>%
+  full_join(tjk_level_7, by = join_by(orgunit_6_uid, orgunit_6), multiple = "all") %>% select(sort(colnames(.))) %>%
   select(orgunit_5, orgunit_5_uid, orgunit_6, orgunit_6_uid)
 
 #merge with data
