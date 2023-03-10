d8ccca799b531cb3c401cdd216ead9578031edaf
aditiarunmozhi <aditiarunmozhi@gmail.com>
2023-03-10 15:31:24 -0500

    psnu updates

--- custom_indicator/Scripts/orgunits_phl_psnu.R
+++ custom_indicator/Scripts/orgunits_phl_psnu.R
@@ -1,4 +1,5 @@
 #psnu level 4
 table(phl$orgunit_level)
 
+phl_merge_psnu <- phl %>% mutate(psnu = "Philippines", psnu_uid = "p1E1K4MWGpa") %>% select(-c(contains("parent")))
 #psnu level 4 is the country level so check that this is correct
