8f7bc100835e34975743f149864ae71dcda57a1b
aditiarunmozhi <aditiarunmozhi@gmail.com>
2023-03-30 12:02:42 -0400

    updates to functions

--- custom_indicator/Scripts/functions.R
+++ custom_indicator/Scripts/functions.R
@@ -2,24 +2,44 @@
 #org unit table functions
 orgunit_clean <- function(df) {df %>% select(-c(regionorcountry_code, regionorcountry_name, moh_id, orgunit_code))}
 
-orgunit_level_sep <- function(df, lev, new_col_1, new_col_uid_1, new_col_2, new_col_uid_2) {(df %>% filter(orgunit_level == lev) %>% 
-                                                                                               rename({{new_col_1}} := orgunit_parent, {{new_col_uid_1}} := orgunit_parent_uid, {{new_col_2}} := orgunit_name, {{new_col_uid_2}} := orgunit_uid) %>%
-                                                                                               select(-orgunit_level))}
+orgunit_level_sep <- function(df, lev, new_col_1, new_col_uid_1, new_col_2, new_col_uid_2) {
+  (df %>% filter(orgunit_level == lev) %>% 
+     rename({{new_col_1}} := orgunit_parent, {{new_col_uid_1}} := orgunit_parent_uid, 
+            {{new_col_2}} := orgunit_name, {{new_col_uid_2}} := orgunit_uid) %>%
+     select(-orgunit_level))}
 
-orgunit_table_join <- function(level_x, level_y, orgunit_x_uid, orgunit_x) {full_join(level_x, level_y, by = join_by({{orgunit_x_uid}}, {{orgunit_x}}), multiple = "all") %>%
-                                                                              select(sort(colnames(.)))}
+orgunit_table_join <- function(level_x, level_y, orgunit_x_uid, orgunit_x) {
+  full_join(level_x, level_y, by = join_by({{orgunit_x_uid}}, {{orgunit_x}}), multiple = "all") %>%
+    select(sort(colnames(.)))}
 
 #org unit list function
-list_orgs_func <- function(ou) {df_ous %>% 
+country_list <- function() {
+  countries <- complete_clean_data %>% group_by(country) %>% summarise(.groups = "drop")
+  countries <- c(countries$country)
+}
+
+orgs_func <- function(ou) {
+  org_url <- "https://www.datim.org/api/sqlViews/DataExchOUs/data?format=json"
+  load_secrets()
+  
+  df_ous <- grabr::get_outable(
+    username = glamr::datim_user(), 
+    password = glamr::datim_pwd())
+  list_orgs <- df_ous %>% 
     filter(country %in% c(ou)) %>% 
     pull(country_iso) %>% 
     paste0(org_url, "&var=OU:", ., "&paging=false") %>% 
     httr::GET(httr::authenticate(user = glamr::datim_user(), 
                                  password = glamr::datim_pwd())) %>% 
     httr::content("text") %>% 
-    jsonlite::fromJSON(flatten=TRUE) }
-
-df_orgs_func <- function() {tibble::as_tibble(list_orgs$listGrid$rows, .name_repair = "unique") %>% 
+    jsonlite::fromJSON(flatten=TRUE)
+  
+  df_orgs <- tibble::as_tibble(list_orgs$listGrid$rows, .name_repair = "unique") %>% 
     setNames(list_orgs$listGrid$headers$name) %>% 
     rename_with(.cols = contains("internal_id"),
-                .fn = ~str_replace(., "internal_id", "uid")) }
+                .fn = ~str_replace(., "internal_id", "uid"))
+}
+
+orgunit_level_list <- function(levels) {
+  df_orgs %>% filter(orgunit_level %in% levels)
+}
--- custom_indicator/Scripts/orgunits_lao.R
+++ custom_indicator/Scripts/orgunits_lao.R
@@ -1,30 +1,16 @@
 
 # obtain list of countries from cleaned infolink data ---------------------
-countries <- complete_clean_data %>% group_by(country) %>% summarise(.groups = "drop")
-countries <- c(countries$country)
-
+countries <- country_list()
 
 # get ou tableD from DATIM -------------------------------------------------
 
-org_url <- "https://www.datim.org/api/sqlViews/DataExchOUs/data?format=json"
-
-load_secrets()
-
-
-df_ous <- grabr::get_outable(
-  username = glamr::datim_user(), 
-  password = glamr::datim_pwd(),
-)
-df_ous
-
 #USE PURR TO ITERATE AND PULL LIST FOR ALL COUNTRIES
 
-list_orgs <- list_orgs_func("Laos")
-df_orgs <- df_orgs_func()
+df_orgs <- orgs_func("Laos")
 
 max(df_orgs$orgunit_level)
 
-lao_5_7 <- df_orgs %>% filter(orgunit_level %in% c(5, 6, 7)) %>% print()
+lao_6_7 <- orgunit_level_list(c(6,7))
 # ultimately bind rows so that we can merge by country, orgunit_pa --------
 
 # write_csv(df_orgs, "Data/laos_orgunits.csv")
